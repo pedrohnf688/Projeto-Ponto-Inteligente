d0a38bc7ffbf8751b6e8690c405d0cfe03d47fe7
pedrohnf <pedrohnf2014@gmail.com>
2019-03-09 20:40:12 -0300

    Flyway

--- src/main/java/com/pedrohnf688/api/ProjetoPontoInteligenteApplication.java
+++ src/main/java/com/pedrohnf688/api/ProjetoPontoInteligenteApplication.java
@@ -2,8 +2,10 @@ package com.pedrohnf688.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.pedrohnf688.api")
 public class ProjetoPontoInteligenteApplication {
 
 	public static void main(String[] args) {
